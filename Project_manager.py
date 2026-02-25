d46c0385e116d0ca6062c899139fab67732fd779
Pragyan Panta <pragyanpanta817@gmail.com>
2026-02-25 15:35:04 +0545

    Added mark task as completed feature

--- Project_manager.py
+++ Project_manager.py
@@ -1,114 +1,133 @@
 import ast 
 
 project_list=[]
-def menu():
+def load_task():
+    try:
+        with open("project_file.txt","r",encoding="utf-8") as file:
+            content_list=file.readlines()
+            for i in content_list:
+                stripped_i=i.strip()
+                dict_i=ast.literal_eval(stripped_i)
+                project_list.append(dict_i)
+            menu()
 
-    def add_task():
+    except FileNotFoundError:
+        print("File does the exist")
+        menu()
 
-        while True: #asking the value again and again
-            #task ID
-            while True:
-                try:
-                    task_id=int(input("enter the task nid in number"))
-                    try:
-                        with open("project_file.txt","r", encoding="utf-8") as file:
-                            content=file.readlines()
-                            id_exist=False
-                            for i in content:
-                                stripped_content=i.strip()
-                                dict_content=ast.literal_eval(stripped_content)
-                                if dict_content["Id"]==task_id:
-                                    id_exist= True
-                            if id_exist:
-                                print("ID already exists")
-                                continue
-                            else:
-                                break
-
-                    except FileNotFoundError:
-                        if any(i["Id"]==task_id for i in project_list ):
-                            print("Id is already present,please enter again")
-                            continue
-                        else:
+def add_task():
+    while True: #asking the value again and again
+        #task ID
+        while True:
+            try:
+                id_present=False
+                task_id=int(input("enter the task nid in number"))
+                for i in project_list:
+                    if i["Id"]==task_id:
+                        id_present=True
+                        if id_present:
                             break
-                        
-                
-                except ValueError:
-                    print("please enter value in number ")
+                if id_present:
+                    print("Id already present")
                     continue
-            #task name
-            while True:
-                task_name=input("Enter the name of the task ")
-                if task_name.strip()=="":
-                    print("Whitespaces and empty string Not allowed")
+                break
+                
+            except ValueError:
+                print("please enter value in number ")
+                continue
+        #task name
+        while True:
+            task_name=input("Enter the name of the task ")
+            if task_name.strip()=="":
+                print("Whitespaces and empty string Not allowed")
+                continue
+            else:
+                break
+        
+        #task status
+        while True:
+            task_status=input("enter the status: ")
+            if not task_status:
+                task_status="pending"
+                break
+            elif task_status.strip()=="":
+                print(" WHITESPACES ARE INVALID! ")
+                continue
+            break
+            
+        
+        dictionary={"Id":task_id,"Name":task_name,"Status":task_status}
+        while True:
+            try:
+                task_choice=int(input("do you want to add another task\n 1. Yes\n 2. No"))
+                if task_choice>2 or task_choice<1:
+                    print("please enter valid number for option")
                     continue
                 else:
                     break
-            
-            #task status
-            while True:
-                task_status=input("enter the status: ")
-                if not task_status:
-                    task_status="pending"
-                    break
-                elif task_status.strip()=="":
-                    print(" WHITESPACES ARE INVALID! ")
-                    continue
-                break
-                
-            
-            dictionary={"Id":task_id,"Name":task_name,"Status":task_status}
-            while True:
-                try:
-                    task_choice=int(input("do you want to add another task\n 1. Yes\n 2. No"))
-                    if task_choice>2 or task_choice<1:
-                        print("please enter valid number for option")
-                        continue
-                    else:
-                        break
-                except ValueError:
-                    print("please enter the correct number")
-                    continue
-            
-            if task_choice==1:
-                project_list.append(dictionary)
+            except ValueError:
+                print("please enter the correct number")
                 continue
-            else:
-                project_list.append(dictionary)
-                break
-        menu()
+        
+        if task_choice==1:
+            project_list.append(dictionary)
+            continue
+        else:
+            project_list.append(dictionary)
+            break
+    menu()
     
-    
-    def view_task():
-        try:
-            with open("project_file.txt","r",encoding="utf-8") as file:
-                print("Id\t Name\t Status")
-                for line in file:
-                    dict_line=ast.literal_eval(line)
-                    print(f"{dict_line["Id"]}\t{dict_line["Name"]}\t{dict_line["Status"]}\n")
-            menu()
-        except FileNotFoundError:
-            print("File not found")
-            menu()
-    
-    
-    def save_task(project_list_p):
-        try:
-            with open("project_file.txt","a",encoding="utf-8") as file:
-                for i in project_list_p:
-                    file.write(f"{str(i)}\n")
-        except FileNotFoundError:
-            with open("project_file.txt","w",encoding="utf-8") as file:
-                for i in project_list_p:
-                    file.write(f"{str(i)}\n")
 
+def view_task():
+    try:
+        with open("project_file.txt","r",encoding="utf-8") as file:
+            print("Id\t Name\t Status")
+            for line in file:
+                dict_line=ast.literal_eval(line)
+                print(f"{dict_line["Id"]}\t{dict_line["Name"]}\t{dict_line["Status"]}\n")
+        menu()
+    except FileNotFoundError:
+        print("File not found")
+        menu()
+
+def save_task(project_list_p):
+    
+    with open("project_file.txt","w",encoding="utf-8") as file:
+        for i in project_list_p:
+            file.write(f"{str(i)}\n")
+    
+    
+
+def mark_task_complete():
+    while True:
+        try:
+            task_id=int(input("enter the id of the task"))
+            id_in_list=False
+            for i in project_list:
+                if i["Id"]==task_id:
+                    i["Status"]="completed"
+                    print("Task completed")
+                    id_in_list=True
+                    break
+            if id_in_list==False:
+                print("id not in list")
+                continue
+            elif id_in_list:
+                save_task(project_list)
+                break
+        except ValueError:
+            print("Please enter the number")
+            continue
+
+def menu():
     while True:
         try:
             menu_choice=int(input('''enter the option as a number:
                                   1.Add Task
                                   2.View Task
-                                  3.save and exit'''))
-            if menu_choice>3 or menu_choice<1:
+                                  3.save and exit
+                                  4.mark task completed'''))
+            if menu_choice>4 or menu_choice<1:
                 print("Enter the valid option number")
                 continue
             break
@@ -122,5 +141,6 @@ def menu():
         view_task()
     elif menu_choice==3:
         save_task(project_list)
-
-menu()
+    elif menu_choice==4:
+        mark_task_complete()
+load_task()
